d5a995b0f1822e07662acb73d131abd0e4c92c87
Borna Biro <43830307+BornaBiro@users.noreply.github.com>
2023-10-10 13:48:23 +0200

    Update ESP32_GPIO_Test.ino

--- ESP32_GPIO_Tester/ESP32_GPIO_Test/ESP32_GPIO_Test.ino
+++ ESP32_GPIO_Tester/ESP32_GPIO_Test/ESP32_GPIO_Test.ino
@@ -1,4 +1,7 @@
+// GPIO LUT table. Write here pins that are allowed to be tested.
 uint8_t gpios[] = {0, 2, 4, 5, 12, 13, 14, 15, 25, 26, 27, 33, 32, 18, 19, 23};
+
+// Input only pins.
 uint8_t gpiosInputOnly[] = {34, 35, 36, 39};
 
 void setup()
@@ -6,23 +9,34 @@ void setup()
     Serial.begin(115200);
     Serial.println("Send the GPIO number to set it to HIGH state (or read the state for input only pins");
 
+    // Set all GPIOs to LOW.
     clearAllGPIOs();
 }
 
-void loop() {
+void loop()
+{
+    // Check if you got something on the UART.
     if (Serial.available())
     {
+        // Try to parse it.
         int i = Serial.parseInt();
+
+        // If it's a valid number,  try to change the state on this GPIO.
         if (i != 0)
         {
+            // Set all pins to the LOW state.
             clearAllGPIOs();
+
+            // Check if the pin is input only pin.
             if (i < 34)
             {
+                // If the pin has output functionality, set the HIGH state.
                 Serial.printf("GPIO %d is set to HIGH\r\n", i);
                 digitalWrite(i, HIGH);
             }
             else
             {
+                // If the pin is input only, read the state of the pin
                 Serial.printf("GPIO %d is %d\r\n", i, digitalRead(i));
             }
         }
