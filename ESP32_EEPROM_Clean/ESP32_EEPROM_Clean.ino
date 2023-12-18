d72d45905f9a3eb1bfa27bdf0377ba9709b47f7d
Borna Biro <43830307+BornaBiro@users.noreply.github.com>
2023-12-18 09:19:04 +0100

    Create ESP32_EEPROM_Clean.ino

---
+++ ESP32_EEPROM_Clean/ESP32_EEPROM_Clean.ino
@@ -0,0 +1,37 @@
+// Include ESP32 EEPROM Library
+#include "EEPROM.h"
+
+// Set the EEPROM size in bytes.
+#define EEPROM_SIZE 512
+
+// Fill EEPROM with this vlaue.
+#define EEPROM_FILL_VALUE 0xFF
+
+void setup()
+{
+  // Init the Serial communication at 115200 bauds (for messages).
+  Serial.begin(115200);
+
+  // Send message.
+  Serial.print("EEPROM cleaning...");
+
+  // Init EEPROM library.
+  EEPROM.begin(EEPROM_SIZE);
+
+  // Fill the memory with defined value.
+  for (int i = 0; i < EEPROM_SIZE; i++)
+  {
+      EEPROM.write(i, 0);
+  }
+
+  // Send changes to the EEPROM.
+  EEPROM.commit();
+
+  // Send message for the user.
+  Serial.println("done!");
+}
+
+void loop()
+{
+   // Nothing
+}
